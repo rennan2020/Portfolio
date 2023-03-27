e4ef09b7a8fe04d89a017c2da0998c6d61c67318
Rennan Reis <rennanreis2012@gmail.com>
2023-03-26 21:22:08 -0300

    todas alterações na nova página

--- JavaScrip.js
+++ JavaScrip.js
@@ -1,9 +1,14 @@
-<script>
-  const navbarToggle = document.querySelector('.navbar-toggle');
-  const navbarMenu = document.querySelector('nav ul');
 
-  navbarToggle.addEventListener('click', () => {
-    navbarToggle.classList.toggle('active');
-    navbarMenu.classList.toggle('active');
-  });
-</script>
+function leiaMais() {
+  var maisTexto=document.getElementById("mais");
+  var btnLeiaMais=document.getElementById("btnLeiaMais");
+
+  if(btnLeiaMais.style.display === "none"){
+    maisTexto.style.display="inline";
+    btnLeiaMais.innerHTML="Clique aqui";
+  }else{
+    maisTexto.style.display="none";
+    btnLeiaMais.innerHTML="Manutenção e suporte em informática"
+  }
+  
+}
